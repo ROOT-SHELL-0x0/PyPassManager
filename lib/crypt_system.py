6051ffaf23b63942b1fcb1af2ee22fedfdf5dac7
ROOT <root.exe.sh@gmail.com>
2025-07-29 03:05:17 +0300

    Шифр Вернама заменён на Fermet

--- lib/crypt_system.py
+++ lib/crypt_system.py
@@ -1,14 +1,47 @@
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from base64 import urlsafe_b64encode
+import os
+
 class crypt_system:
 	def __init__(self):
 		None
 
 	def crypt_data(self,data,key):
-		ciphertext = ''.join(chr(ord(p) ^ ord(k)) for p, k in zip(data, key))
-		return ciphertext
+
+		password = key.encode()  
+		salt = b"IOIidisiEO-eJKDKJdufiorjf"
+		kdf = PBKDF2HMAC(
+			algorithm=hashes.SHA256(),
+			length=32,
+			salt=salt,
+			iterations=100000,
+			backend=default_backend())
+		key = urlsafe_b64encode(kdf.derive(password))
+
+		cipher=Fernet(key)
+		return cipher.encrypt(data.encode()).decode()
+		
+
+
+
+
 
 	def decrypt_data(self,data,key):
-		decrypted_text = ''.join(chr(ord(c) ^ ord(k)) for c, k in zip(data, key))
-		return decrypted_text
+		password = key.encode()  
+		salt = b"IOIidisiEO-eJKDKJdufiorjf"
+		kdf = PBKDF2HMAC(
+			algorithm=hashes.SHA256(),
+			length=32,
+			salt=salt,
+			iterations=100000,
+			backend=default_backend())
+		key = urlsafe_b64encode(kdf.derive(password))
 
+		cipher=Fernet(key)
+		return cipher.decrypt(data.encode()).decode()
+		
 
 
